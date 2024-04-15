9853840d796581995e24728b8383727c380d8783
Jiahui0123 <130739768+Jiahui0123@users.noreply.github.com>
2024-04-15 22:50:39 +0800

    fix listdetail

--- src/components/ListDetail.js
+++ src/components/ListDetail.js
@@ -4,36 +4,54 @@ import { useState } from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ListDetail = () => {
-    const [PressState, setPressState] = useState(false);
-    let checkbtm = PressState ? <MaterialCommunityIcons name={'check-circle'} size={20} color={'#4ECB71'} />
+    const [PressState1, setPressState1] = useState(false);
+    const [PressState2, setPressState2] = useState(false);
+    let checkbtm = PressState1 ? <MaterialCommunityIcons name={'check-circle'} size={20} color={'#4ECB71'} />
+        : <MaterialCommunityIcons name={'checkbox-blank-circle-outline'} size={20} color={'#fff'} />;
+    let checkbtm2 = PressState2 ? <MaterialCommunityIcons name={'check-circle'} size={20} color={'#4ECB71'} />
         : <MaterialCommunityIcons name={'checkbox-blank-circle-outline'} size={20} color={'#fff'} />;
     return (
         <ScrollView contentContainerStyle={styles.contentContainer}>
+            <Text style={styles.date}>2024.4.14—————————————————————————</Text>
             <View style={styles.CardContainer}>
                 <Image
                     source={require('../images/IMG_02001.jpg')}
                     style={styles.image}
                 />
-                <Text style={styles.itemText}>food</Text>
-                <Text style={styles.expText}>EXP:2024.2.30</Text>
-                <Pressable onPress={() => setPressState(!PressState)}>
+                <Text style={styles.itemText}>BlackTea</Text>
+                <Text style={styles.expText}>EXP:2024.4.24</Text>
+                <Pressable onPress={() => setPressState1(!PressState1)}>
                     {checkbtm}
+                </Pressable>
+            </View>
+            <View style={styles.CardContainer}>
+                <Image
+                    source={require('../images/IMG_02001.jpg')}
+                    style={styles.image}
+                />
+                <Text style={styles.itemText}>Milk</Text>
+                <Text style={styles.expText}>EXP:2024.4.21</Text>
+                <Pressable onPress={() => setPressState2(!PressState2)}>
+                    {checkbtm2}
                 </Pressable>
             </View>
         </ScrollView>
     )
 };
 const styles = StyleSheet.create({
+    date: {
+        color: "#7A929B",
+        paddingTop: 30,
+    },
     contentContainer: {
-        alignItems:'center',
+        alignItems: 'center',
     },
     CardContainer: {
         flexDirection: 'row',
-        width:'85%',
-
+        width: '85%',
         height: 50,
         backgroundColor: "#001B24",
-        marginTop: 20,
+        marginTop: 10,
         paddingHorizontal: 20,
         alignItems: 'center',
         justifyContent: 'space-between',
