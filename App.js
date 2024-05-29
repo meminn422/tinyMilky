db42fcdb3d77a4b9bdc5b6cf9e9843b831f6e885
Jiahui0123 <s111119035@stu.ntue.edu.tw>
2024-05-29 15:46:01 +0800

    bottomTab adjust

--- App.js
+++ App.js
@@ -10,12 +10,4 @@ const App = () => {
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   }
-// });
-
 export default App;
